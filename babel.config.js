063e570c46572b02318e6805882f7fff81bf7992
bagussatoto <bagusbudisatoto2001@gmail,com>
2023-07-23 21:47:31 +0700

    gabut

--- babel.config.js
+++ babel.config.js
@@ -2,3 +2,15 @@ module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [['react-native-reanimated/plugin']]
 };
+
+
+
+
+// NSDVNLSDNBLISDBSBIJHJEOJGEKGJ;LJAS;LSJG;LAJGPOQ  JESJGPOJEGPOJEPOGJEPJAmfljb;ojhpos
+///jgjpewjgpoewjgpoejwpogjewp hneihgiejgewjgoewjgoewjogjwoejgpowjpoewogjpw
+//jpohjewjpojewpojewgjewjgewjjjejpojewphjpehawwgha  ahghfwhgo///\
+///djasfhqwoaghoiwhjgoihwhoiwhoihqwhtgwqhgihwqoigh
+//egjpqwjgpoqjwgpojqwpogjwpoqgjjepjgpowjgpqjwgp
+//jepgjpojwgpowjgpowjpogjwpojpqwdfjbpsjpds/
+//jsdpjpesdjghpsjphjdspojhdposjhdsjpohjsdpj
+//jdspojhposdjhdsjhspjpjspohj
