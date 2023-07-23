22663ec222236c0272dd125c887194309f837c35
bagussatoto <bagusbudisatoto2001@gmail,com>
2023-07-23 09:27:44 +0700

    commit

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -8,7 +8,10 @@ import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import babelConfig from '../babel.config';
+import { AppRegistry } from 'react-native';
 
 it('renders correctly', () => {
     renderer.create(<App />);
+    babelConfig(AppRegistry)
 });
