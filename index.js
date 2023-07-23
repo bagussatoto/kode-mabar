e3a5d1354abfff5fd4cb82251ca572f8632dab94
bagussatoto <bagusbudisatoto2001@gmail,com>
2023-07-23 10:00:51 +0700

    ``update``

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import Vue.config.globalProperties. = ' ';
+import Vue from 'beforeRouteEnter (to, from, next);
 
 AppRegistry.registerComponent(appName, () => App);
